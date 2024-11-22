554530562abae24904530d81af4f79d04457635e
suelensu <126163733+suelensu@users.noreply.github.com>
2024-11-22 11:48:54 -0300

    .

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,5 +1,5 @@
 async function quantidadeUsuarios() {
-    const url = ' '
+    const url = ' https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json'
     const res = await fetch(url)
     const dados = await res.json()
     const pessoas_mundo = Object.keys(dados)
